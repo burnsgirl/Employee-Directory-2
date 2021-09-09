0a60159945461d2c7de7f953269c75afb54efc3e
Dauphine Burns <dauphineburns@gmail.com>
2021-09-09 13:07:58 -0400

    deleted dup named in readme

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Employee from "./generator";
 
+
 function Dropdown() {
+    const handleClick (e, items) {
+        console.log(`${items}`)
+    }
     return (
     //     <div class="dropdown">
     // <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -12,9 +16,10 @@ function Dropdown() {
     //     <a class="dropdown-item" href="#">Alphabetical order</a>
     // </div>
     // </div>
-
+        <>
         <button onClick={(e) => handleClick(e, {items.location.state})}>Country</button>
         <button onClick={(e) => handleClick(e, {items.dob.age})}>Age</button>
+        </>
     )
 }
 
