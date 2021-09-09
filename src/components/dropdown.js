496cf5c5caa0869ead13cc426040139a00fe94fe
Dauphine Burns <dauphineburns@gmail.com>
2021-09-09 10:33:08 -0400

    changed search parameters

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,15 +1,15 @@
 import React from "react";
+import Employee from "./generator";
 
-function Dropdown() {
+function Dropdown(props) {
     return (
         <div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown button
+        Sort by
     </button>
     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
+        <a class="dropdown-item" href="#">Country</a>
+        <a class="dropdown-item" href="#">Alphabetical order</a>
     </div>
     </div>
     )
