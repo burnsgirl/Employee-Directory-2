b6294a9ee3302cf34a1688522b8a4ff7d4a0f82f
Dauphine Burns <dauphineburns@gmail.com>
2021-09-09 10:28:09 -0400

    added dropdown to app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './components/title';
 import Employee from './components/generator';
+import Dropdown from './components/dropdown';
 
 function App() {
   return (
     <body>
       <Title />
+      <Dropdown />
       <Employee />
 
     </body>
