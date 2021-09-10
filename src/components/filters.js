512d1a41b51d4b9c3e7311b74632bd6a75502392
Dauphine Burns <dauphineburns@gmail.com>
2021-09-10 11:40:10 -0400

    added sort basics

--- src/components/filters.js
+++ src/components/filters.js
@@ -5,6 +5,41 @@ import Api from "./utils/api";
 
 class Filter extends Component {
 
+    state = {
+        sortChoice: '',
+        filterChoice: '',
+        results: [],
+        baseResults: []
+    };
+
+    componentDidMount() {
+        this.searchApi("&results=30");
+    };
+
+    searchApi = data => {
+        Api.search(data)
+        .then(res => {
+            this.setState({results: res.data.results, baseResults: res.data.results})
+        })
+        .catch(err => console.log(err))
+    };
+
+    handleSortChange = e => {
+        console.log('sort function');
+        const value = e.target.value;
+        this.setState({
+            sortChoice: value, 
+        })
+        this.sortData(e.target.value);
+
+        
+        if (this.state.filterChoice===''){
+            return;
+        } else {
+            this.filterData(this.state.filterChoice, this.state.results);
+        }
+    };
+    
 }
 
 export default Filter;
