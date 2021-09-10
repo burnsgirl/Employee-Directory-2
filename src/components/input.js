bbb529e0e5fe588469a415997613ed0928456dde
Dauphine Burns <dauphineburns@gmail.com>
2021-09-10 11:58:07 -0400

    fixed imports

--- src/components/input.js
+++ src/components/input.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import InputInfo from './inputinfo';
 
 
 
 function Input(props) {
     return (
         <div class='py-3'>
-            <InputDetail onChange={props.handleSortChange} 
+            <InputInfo onChange={props.handleSortChange} 
             type='sort' 
             defaultVal='' defaultDesc='Select Sort Order' 
             val1='last' val1Desc='Last Name' 
             val2='country' val2Desc='Country'/>
-            <InputDetail onChange={props.handleFilterChange} 
+            <InputInfo onChange={props.handleFilterChange} 
             type='filter' 
             defaultVal='' defaultDesc='Select Age Range' 
             val1='yage' val1Desc='Ages 20-40' 
