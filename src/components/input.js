36b2515de599b16ea99e41f82efb5b299ae43376
Dauphine Burns <dauphineburns@gmail.com>
2021-09-10 11:56:57 -0400

    added search criteria

--- src/components/input.js
+++ src/components/input.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import InputInfo from './inputinfo';
+
 
 
 function Input(props) {
     return (
-        
+        <div class='py-3'>
+            <InputDetail onChange={props.handleSortChange} 
+            type='sort' 
+            defaultVal='' defaultDesc='Select Sort Order' 
+            val1='last' val1Desc='Last Name' 
+            val2='country' val2Desc='Country'/>
+            <InputDetail onChange={props.handleFilterChange} 
+            type='filter' 
+            defaultVal='' defaultDesc='Select Age Range' 
+            val1='yage' val1Desc='Ages 20-40' 
+            val2='oage' val2Desc='Ages 41-60'/>
+        </div>
     )
 }
 
--- src/components/inputinfo.js
+++ src/components/inputinfo.js
@@ -3,7 +3,13 @@ import React from "react";
 
 function InputInfo(props) {
     return (
-       
+        <form class='m-3'>
+        <select name={props.type} id={props.type} onChange={props.onChange}>
+            <option value={props.defaultVal}>{props.defaultDesc}</option>
+            <option value={props.val1}>{props.val1Desc}</option>
+            <option value={props.val2}>{props.val2Desc}</option>
+        </select>
+    </form>
     )
 }
 
