e824222a4dd682f0fa6b0d0cceb56a0035a84c4b
Dauphine Burns <dauphineburns@gmail.com>
2021-09-10 11:48:21 -0400

    added outlines for input and input info

---
+++ src/components/input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import InputInfo from './inputinfo';
+
+
+function Input(props) {
+    return (
+        
+    )
+}
+
+export default Input;
---
+++ src/components/inputinfo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function InputInfo(props) {
+    return (
+       
+    )
+}
+
+export default InputInfo;
