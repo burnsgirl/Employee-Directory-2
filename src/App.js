5312c5dceca7f7c63c7a723e8573665c24135378
Dauphine Burns <dauphineburns@gmail.com>
2021-09-10 11:26:02 -0400

    deleted unness files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/title';
-import Employee from './components/generator';
-import Dropdown from './components/dropdown';
+import Table from './components/table';
+
 
 function App() {
   return (
     <body>
       <Title />
-      <Dropdown />
-      <Employee />
+      
+      <Table />
 
     </body>
 
